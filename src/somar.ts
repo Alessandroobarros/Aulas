d94ba67ba305c5a57a1962fa3dc2d7e6a9868a4c
Alessandro Barros <alex.sandro.jb@hotmail.com>
2020-11-25 15:11:54 -0300

    Estrutura de projetos em pastas separadas.

---
+++ src/somar.ts
@@ -0,0 +1,24 @@
+//Generics
+const numero1 = document.querySelector<HTMLInputElement>('#num1')!
+const numero2 = document.querySelector<HTMLInputElement>('#num2')!
+const formulario = document.querySelector<HTMLFormElement>('form')!
+
+
+formulario.addEventListener('submit', (event: Event) => {
+    event.preventDefault()
+
+    let p = document.querySelector<HTMLParagraphElement>('#mensagem')
+
+    if (!p){
+        p = document.createElement('p')
+        p.id = 'mensagem'
+        formulario.append(p)
+    }
+
+    if (numero1.value && numero2.value){
+        let soma = Number(numero1.value) + Number(numero2.value)
+        p.innerText = `A soma dos numeros é de ${soma}`
+    }else {
+        p.innerText = `Favor informar os dois numero`
+    }
+})
---
+++ src/sorteador.ts
@@ -0,0 +1,28 @@
+const sortear = document.querySelector<HTMLButtonElement>('#sortear')!
+
+sortear.addEventListener('click', () => {
+    const nomes = [
+        "Alessandro",
+        "Aline",
+        "Alef",
+        "Amanda",
+        "Bruno",
+        "Brenda",
+        "Carlos",
+        "Camila",
+        "Daniela",
+        "Geovana",
+        "Julia",
+        "Kendiel",
+        "Vinicius",
+        "valquiria"
+    ]
+     //random soteia numero decimal de 0 a 1, e round arredonta numeros do random para inteiro
+    const resultSorteio = Math.round(Math.random() * (nomes.length - 1))
+
+    const p = document.createElement('p')
+    document.body.append(p)
+
+    p.innerText = nomes[resultSorteio]
+})
+
