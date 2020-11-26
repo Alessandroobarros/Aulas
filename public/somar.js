7791b4f4bedabf678ff6efe262a2cfdfecfddd7a
Alessandro Barros <alex.sandro.jb@hotmail.com>
2020-11-26 14:35:56 -0300

    Expressoes Regulares e validação da data.

--- public/somar.js
+++ public/somar.js
@@ -1,21 +1,22 @@
 "use strict";
-//Generics
-const numero1 = document.querySelector('#num1');
-const numero2 = document.querySelector('#num2');
-const formulario = document.querySelector('form');
-formulario.addEventListener('submit', (event) => {
-    event.preventDefault();
-    let p = document.querySelector('#mensagem');
-    if (!p) {
-        p = document.createElement('p');
-        p.id = 'mensagem';
-        formulario.append(p);
-    }
-    if (numero1.value && numero2.value) {
-        let soma = Number(numero1.value) + Number(numero2.value);
-        p.innerText = `A soma dos numeros é de ${soma}`;
-    }
-    else {
-        p.innerText = `Favor informar os dois numero`;
-    }
-});
+{ //Generics
+    const numero1 = document.querySelector('#num1');
+    const numero2 = document.querySelector('#num2');
+    const formulario = document.querySelector('form');
+    formulario.addEventListener('submit', (event) => {
+        event.preventDefault();
+        let p = document.querySelector('#mensagem');
+        if (!p) {
+            p = document.createElement('p');
+            p.id = 'mensagem';
+            formulario.append(p);
+        }
+        if (numero1.value && numero2.value) {
+            let soma = Number(numero1.value) + Number(numero2.value);
+            p.innerText = `A soma dos numeros é de ${soma}`;
+        }
+        else {
+            p.innerText = `Favor informar os dois numero`;
+        }
+    });
+}
--- src/documentos-exigidos.ts
+++ src/documentos-exigidos.ts
@@ -1,35 +1,79 @@
 {
     const nome = document.querySelector<HTMLInputElement>('#nome')!
+    const email = document.querySelector<HTMLInputElement>('#email')!
     const nascimento = document.querySelector<HTMLInputElement>('#nascimento')!
     const sexo = document.querySelector<HTMLSelectElement>('#sexo')!
     const termosAceitos = document.querySelector<HTMLInputElement>('#termosAceitos')!
     const formulario = document.querySelector<HTMLFormElement>('form')!
     const resposta = document.querySelector<HTMLDivElement>('#resposta')!
 
+    formulario.noValidate = true
+
     formulario.addEventListener('submit', (e: Event) =>{
         e.preventDefault()
 
-        if (!nome.value){
+        let valorNome = nome.value.trim()
+
+        if (!valorNome){
             resposta.innerText = 'O campo nome é obrigatório'
             resposta.className = 'negative'
+            nome.focus()
+            return
+        }
+
+        const regexNome = /\w+\s\w/g
+
+        
+        if (!regexNome.test(valorNome)){
+            resposta.innerText = 'Informe seu nome completo !'
+            resposta.className = 'negative'
+            nome.focus()
+            return
+        }
+
+        if (!email.value){
+            resposta.innerText = 'O campo e-mail é obrigatório'
+            resposta.className = 'negative'
+            email.focus()
+            return
+        }
+
+        const regexEmail = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g
+
+        if(!regexEmail.test(email.value)){
+            resposta.innerText = 'Formato de e-mail invalido'
+            resposta.className = 'negative'
+            email.focus()
             return
         }
 
         if (!nascimento.value){
             resposta.innerText = 'O campo nascimento é obrigatório'
             resposta.className = 'negative'
+            nascimento.focus()
             return
         }
 
-        if (!sexo.value){
-            resposta.innerText = 'O campo sexo é obrigatório'
+        if (!nascimento.value){
+            resposta.innerText = 'O campo nascimento é obrigatório'
             resposta.className = 'negative'
+            nascimento.focus()
+            return
+        }
+
+        const dataNascimento = new Date(`${nascimento.value}T00:00:00`)
+
+        if (Date.now() - Number(dataNascimento) <= 0){
+            resposta.innerText = 'A data de nascimento tem quer ser anterior a data atual !'
+            resposta.className = 'negative'
+            sexo.focus()
             return
         }
 
         if (!termosAceitos.checked){
             resposta.innerText = 'É necessario aceitar os termos antes de prosseguir'
             resposta.className = 'negative'
+            termosAceitos.focus()
             return
         }
 
