db8dd81d5b2375df2382c6f545fd064f5ecf17bf
Alessandro Barros <alex.sandro.jb@hotmail.com>
2020-11-27 16:56:23 -0300

    Herança, encapsulamento, abstração

--- src/exemplos-oop.ts
+++ src/exemplos-oop.ts
@@ -1,137 +1,141 @@
-let nome1 = 'Erick'
-let nome2 = String('Erick')
+{
+    let nome1 = 'Erick'
+    let nome2 = String('Erick')
 
 
 
-enum Sexo {
-    Feminino = 'f',
-    Masculino = 'm'
-}
-
-type professor = {
-    nome: string,
-    sexo: Sexo,
-}
-
-// type aluno = {
-//     ra: number | number,
-//     nome: string,
-//     sexo: Sexo,
-//     curso: string,
-// }
-
-// type alunoComMetodos = aluno &{
-//     dizerNome(): String,
-//     mudarSexo(): void
-
-// }
-
-class Aluno {
-    ra: string | number
-    nome: string
-    sexo: Sexo
-    curso: string
-
-    constructor(ra: string | number, nome: string, sexo: Sexo, curso: string) {
-        this.ra = ra
-        this.nome = nome
-        this.sexo = sexo
-        this.curso = curso
+    enum Sexo {
+        Feminino = 'f',
+        Masculino = 'm'
     }
 
-    dizerNome() {
-        return `Olá eu sou ${this.nome}`
+    type professor = {
+        nome: string,
+        sexo: Sexo,
     }
 
-    mudarSexo() {
-        if (this.sexo == Sexo.Masculino) {
-            this.sexo = Sexo.Feminino
-        } else {
-            this.sexo = Sexo.Masculino
+    // type aluno = {
+    //     ra: number | number,
+    //     nome: string,
+    //     sexo: Sexo,
+    //     curso: string,
+    // }
+
+    // type alunoComMetodos = aluno &{
+    //     dizerNome(): String,
+    //     mudarSexo(): void
+
+    // }
+
+    class Aluno {
+        ra: string | number
+        nome: string
+        sexo: Sexo
+        curso: string
+
+        constructor(ra: string | number, nome: string, sexo: Sexo, curso: string) {
+            this.ra = ra
+            this.nome = nome
+            this.sexo = sexo
+            this.curso = curso
+        }
+
+        dizerNome() {
+            return `Olá eu sou ${this.nome}`
+        }
+
+        mudarSexo() {
+            if (this.sexo == Sexo.Masculino) {
+                this.sexo = Sexo.Feminino
+            } else {
+                this.sexo = Sexo.Masculino
+            }
         }
     }
+
+    const aluno1 = new Aluno(1234, 'Alessandro', Sexo.Masculino, 'Curso Aspira')
+
+    const aluno2 = new Aluno(4567, 'Vitória', Sexo.Feminino, 'Curso Aspira')
+
+    const aluno3 = new Aluno(7890, 'Bruna', Sexo.Feminino, 'Curso Aspira')
+
+    const aluno4 = new Aluno(3955, 'Cidcleison', Sexo.Masculino, 'Curso Aspira')
+
+
+    // const aluno1: alunoComMetodos = {
+    //     ra: 3955,
+    //     nome: 'Alessandro',
+    //     sexo: Sexo.Masculino,
+    //     curso: 'Aspira',
+
+    //     dizerNome(){
+    //         return `Olá eu sou ${this.nome}`
+    //     },
+
+    //     mudarSexo(){
+    //         if(this.sexo == Sexo.Masculino) {
+    //             this.sexo = Sexo.Feminino
+    //         }else{
+    //             this.sexo = Sexo.Masculino
+    //         }
+    //     }
+    // }
+
+    // const aluno2: alunoComMetodos = {
+    //     ra: 1234,
+    //     nome: 'Maisa',
+    //     sexo: Sexo.Feminino,
+    //     curso: 'Aspira',
+
+    //     dizerNome(){
+    //         return `Olá eu sou ${this.nome}`
+    //     },
+
+    //     mudarSexo(){
+    //         if(this.sexo == Sexo.Masculino) {
+    //             this.sexo = Sexo.Feminino
+    //         }else{
+    //             this.sexo = Sexo.Masculino
+    //         }
+    //     }
+    // }
+
+    // const aluno3: aluno = {
+    //     ra: 5678,
+    //     nome: 'teste',
+    //     sexo: Sexo.Masculino,
+    //     curso: 'fora',
+    // }
+
+    // const aluno4: aluno = {
+    //     ra: 9012,
+    //     nome: 'joao',
+    //     sexo: Sexo.Masculino,
+    //     curso: 'fora',
+    // }
+    const alunos = [aluno1, aluno2, aluno3, aluno4]
+    console.log(alunos)
+
+    console.log(aluno1.dizerNome())
+    console.log(aluno2.dizerNome())
+
+    // const prof: professor = {
+    //     nome: 'Erick',
+    //     sexo: Sexo.Masculino
+    // }
+
+
+    // declare global{
+    //     interface String{
+    //         removeDuplicateChars(): string
+    //     }    
+    // }
+
+    // String.prototype.removeDuplicateChars = function (){
+    //     return this.replace(/ +/g, ' ')
+    // }
+
+    // let nome = 'Erick          teste'
+    // console.log(nome.removeDuplicateChars())
+
 }
-
-const aluno1 = new Aluno(1234, 'Alessandro', Sexo.Masculino, 'Curso Aspira')
-
-const aluno2 = new Aluno(4567, 'Vitória', Sexo.Feminino, 'Curso Aspira')
-
-const aluno3 = new Aluno(7890, 'Bruna', Sexo.Feminino, 'Curso Aspira')
-
-const aluno4 = new Aluno(3955, 'Cidcleison', Sexo.Masculino, 'Curso Aspira')
-
-
-// const aluno1: alunoComMetodos = {
-//     ra: 3955,
-//     nome: 'Alessandro',
-//     sexo: Sexo.Masculino,
-//     curso: 'Aspira',
-
-//     dizerNome(){
-//         return `Olá eu sou ${this.nome}`
-//     },
-
-//     mudarSexo(){
-//         if(this.sexo == Sexo.Masculino) {
-//             this.sexo = Sexo.Feminino
-//         }else{
-//             this.sexo = Sexo.Masculino
-//         }
-//     }
-// }
-
-// const aluno2: alunoComMetodos = {
-//     ra: 1234,
-//     nome: 'Maisa',
-//     sexo: Sexo.Feminino,
-//     curso: 'Aspira',
-
-//     dizerNome(){
-//         return `Olá eu sou ${this.nome}`
-//     },
-
-//     mudarSexo(){
-//         if(this.sexo == Sexo.Masculino) {
-//             this.sexo = Sexo.Feminino
-//         }else{
-//             this.sexo = Sexo.Masculino
-//         }
-//     }
-// }
-
-// const aluno3: aluno = {
-//     ra: 5678,
-//     nome: 'teste',
-//     sexo: Sexo.Masculino,
-//     curso: 'fora',
-// }
-
-// const aluno4: aluno = {
-//     ra: 9012,
-//     nome: 'joao',
-//     sexo: Sexo.Masculino,
-//     curso: 'fora',
-// }
-const alunos = [aluno1, aluno2, aluno3, aluno4]
-console.log(alunos)
-
-console.log(aluno1.dizerNome())
-console.log(aluno2.dizerNome())
-
-// const prof: professor = {
-//     nome: 'Erick',
-//     sexo: Sexo.Masculino
-// }
-
-
-declare global{
-    
-}
-String.prototype.removeDuplicateChars = function (){
-    return this.replace(/ +/g, ' ')
-}
-
-let nome = 'Erick          teste'
-console.log(nome.removeDuplicateChars())
-
-
---
+++ src/maisexemplos-oop.ts
@@ -0,0 +1,73 @@
+namespace Escola {
+    export enum Sexo {
+        Masculino = 'm',
+        Feminino = 'f'
+    }
+
+    export class Pessoa {
+        nome: string
+        sexo: Sexo
+
+        constructor(nome: string, sexo: Sexo){
+            this.nome = nome
+            this.sexo = sexo
+        }
+
+        apresentarSe () {
+            const artigo = this.sexo === Sexo.Masculino ? 'o' : 'a'
+            return `Olá, eu sou ${artigo} ${this.nome}`
+        }
+    }
+
+    export class Aluno extends Pessoa {
+        #ra: number
+        curso: string
+
+        constructor(ra: number, nome: string, sexo: Sexo, curso: string) {
+            super(nome, sexo)
+            if (ra == 1234 || ra == 5678 || ra == 9012) {
+                this.#ra = ra
+            } else {
+                throw new Error('Formato invalido')
+            }
+            this.curso = curso
+        }
+
+        //Acessors 
+        get ra() {
+            return this.#ra
+        }
+
+        set ra(value: number) {
+            if (value == 1234 || value == 5678 || value == 9012) {
+                this.#ra = value
+            } else {
+                throw new Error('Formato invalido')
+            }
+        }
+
+    }
+
+    export class professor extends Pessoa{ }
+}
+
+{
+    const aluno1 = new Escola.Aluno(1234, 'Alessandro', Escola.Sexo.Masculino, 'Aspira')
+    const aluno2 = new Escola.Aluno(5678, 'Yasmin', Escola.Sexo.Feminino, 'Aspira')
+    const aluno3 = new Escola.Aluno(9012, 'Mikaely', Escola.Sexo.Feminino, 'Aspira')
+    //const aluno4 = new Escola.Aluno(3955, 'jorge', Escola.Sexo.Masculino, 'Aspira')
+
+
+    const alunos = [aluno1, aluno2, aluno3]
+    console.log(alunos)
+
+    console.log(aluno1.apresentarSe())
+    console.log(aluno2.apresentarSe())
+    console.log(aluno3.apresentarSe())
+
+    const prof = new Escola.professor('Erick', Escola.Sexo.Masculino)
+    
+    console.log(prof)
+
+    console.log(prof.apresentarSe())
+}
